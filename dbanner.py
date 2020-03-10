6766eaac073090648b50913db0f2d222b7fe5a14
Özkan Mustafa Akkuş <akkuscw@gmail.com>
2020-03-11 01:32:16 +0300

    Banner

---
+++ dbanner.py
@@ -0,0 +1,22 @@
+from colorama import *
+def bnr():
+    print ('''
+ _____________________________________________________________________
+|_____________________________|DRAFTCHAT|_____________________________|
+''' + Fore.GREEN + ''' ___________                        ''' + Fore.YELLOW + '''  ___________
+''' + Fore.GREEN + '''||         ''' + Style.RESET_ALL + Fore.GREEN + '''||   If Privacy Matters   ''' + Fore.YELLOW + '''||         || ''' + Fore.WHITE + Style.DIM + '''> Via single account      
+''' + Style.RESET_ALL + Fore.GREEN + '''|| ''' + Style.BRIGHT + Back.GREEN + Fore.BLACK + '''SERVER''' +  Style.RESET_ALL + Fore.GREEN + '''  ||         ''' + Style.BRIGHT + Fore.BLUE + '''________       ''' + Style.RESET_ALL + Fore.YELLOW + '''|| ''' + Style.BRIGHT + Back.YELLOW + Fore.BLACK + '''CLIENT''' +  Style.RESET_ALL + Fore.YELLOW + '''  || ''' + Style.DIM + Fore.WHITE + '''> Via single draft
+''' + Style.RESET_ALL + Fore.GREEN + '''||         ''' + Style.RESET_ALL + Fore.GREEN + '''||        ''' + Style.BRIGHT + Fore.BLUE + '''| ---- | \     ''' + Style.RESET_ALL + Fore.RED + '''_''' + Fore.YELLOW + '''||         || ''' + Style.DIM + Fore.WHITE + '''> With AES and DES 
+''' + Style.RESET_ALL + Fore.GREEN + '''||         ''' + Style.RESET_ALL + Fore.GREEN + '''||        ''' + Style.BRIGHT + Fore.BLUE + '''| ---- |__\   ''' + Style.RESET_ALL + Fore.RED + '''/ ''' + Fore.YELLOW + '''||_________|| ''' + Style.DIM + Fore.WHITE + '''> %100 private chat
+''' + Style.RESET_ALL + Fore.GREEN + '''|  + + + +  |        ''' + Style.BRIGHT + Fore.BLUE + '''| -- -- - |  ''' + Style.RESET_ALL + Fore.RED + '''/  ''' + Fore.YELLOW + '''|  + + + +  |
+''' + Fore.GREEN + '''    _|_|_   ''' + Fore.RED + '''\\''' + Style.BRIGHT + Fore.CYAN + '''AES     ''' + Style.RESET_ALL + Style.BRIGHT + Fore.BLUE + '''| -''' + Style.BRIGHT + Back.BLUE + Fore.WHITE + '''DRAFT''' + Style.RESET_ALL + Style.BRIGHT + Fore.BLUE +'''- ''' + Style.BRIGHT + Fore.BLUE + '''| ''' +  Style.RESET_ALL + Fore.RED + '''/''' + Style.BRIGHT + Fore.CYAN + '''AES    ''' + Style.RESET_ALL +Fore.YELLOW + '''_|_|_
+''' + Fore.GREEN + '''   (_____)   ''' + Fore.RED + '''\\''' + Style.BRIGHT + Fore.MAGENTA + '''DES   ''' + Style.RESET_ALL + Style.BRIGHT + Fore.BLUE + '''_|_ - ''' + Style.BRIGHT + Back.BLUE + Fore.WHITE + '''CHAT''' + Style.RESET_ALL + ''' |''' + Style.RESET_ALL + Fore.RED + '''/''' + Style.BRIGHT + Fore.MAGENTA + '''DES    ''' + Style.RESET_ALL + Fore.YELLOW + '''(_____)
+              ''' + Fore.RED + '''\    ''' + Style.BRIGHT + Fore.BLUE + '''/ |,\ ----- |''' + Style.RESET_ALL + '''
+''' + Fore.BLUE + '''       _______ ''' + Fore.RED + '''\__''' + Style.BRIGHT + Fore.BLUE + '''|- ' -|______|''' + Style.RESET_ALL + '''
+''' + Fore.BLUE + '''      |       |''' + Fore.RED + '''/   ''' + Style.BRIGHT + Fore.BLUE + '''\_,_/''' + Style.RESET_ALL + '''                   ''' + Style.BRIGHT + Fore.BLUE + '''~ ''' + Style.RESET_ALL +'''Author : ''' + Style.BRIGHT + Fore.GREEN + '''AkkuS ''' + Style.RESET_ALL + '''@ehakkus
+''' + Fore.BLUE + '''      | ''' + Style.DIM + Back.BLUE + Fore.WHITE + '''TOKEN''' +  Style.RESET_ALL + Fore.BLUE + ''' |    ''' + Fore.RED + '''/    |_________|        ''' + Style.BRIGHT + Fore.BLUE + '''~ ''' + Style.RESET_ALL +'''github.com / '''+ Fore.MAGENTA + '''draftchat
+''' + Fore.BLUE + '''      |_______|   ''' + Fore.RED + '''/      ''' + Style.DIM + Fore.WHITE + '''GMAIL API         ''' + Style.RESET_ALL + Style.BRIGHT + Fore.BLUE + '''~ ''' + Style.RESET_ALL + Style.DIM + Fore.WHITE + '''https://pentest.com.tr
+           ''' + Style.NORMAL + Fore.RED + '''\_____/''' + Style.RESET_ALL + '''
+                                                 Version 1.0 ''' + Style.DIM + Fore.WHITE + '''(Stable)
+_____________________________________________________________________
+          ''')
